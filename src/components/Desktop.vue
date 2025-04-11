0eac39d754e4f357c9eef19de20f52d9bed3205f
Emmanuel Ardoin <emmanuel.ardoin@ypok.com>
2025-04-11 15:59:35 +0200

    d

---
+++ src/components/Desktop.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+
+<template>
+  <div id="desktop"></div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/TaskBar.vue
@@ -0,0 +1,30 @@
+<script setup>
+</script>
+
+<template>
+  <div id="taskbar">
+    <button id="taskbar-start">
+    </button>
+  </div>
+</template>
+
+<style scoped>
+#taskbar {
+  background-color: cadetblue;
+  border-top: gray solid 2px;
+}
+#taskbar-start {
+  background-color: green;
+  text-decoration: none;
+  border: none;
+
+  width: 10%;
+  height: 100%;
+}
+#taskbar-start:hover {
+  background-color: limegreen;
+}
+#taskbar-start:active {
+  background-color: darkgreen;
+}
+</style>
